a1c569b3754c7cbab6dea21371480351f0a7b403
gary-mayfield <gary.mayfield@hotmail.co.uk>
2022-02-13 11:56:08 +0000

    up to chapter 5.

--- com/craftinginterpreters/flick/Scanner.java
+++ com/craftinginterpreters/flick/Scanner.java
@@ -3,7 +3,9 @@ package com.craftinginterpreters.flick;
 import static com.craftinginterpreters.flick.TokenType.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner 
 {
@@ -12,6 +14,28 @@ public class Scanner
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static
+    {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source)
     {
@@ -81,12 +105,26 @@ public class Scanner
                 {
                     number();
                 }
+                else if(isAlpha(c))
+                {
+                    identifier();
+                }
                 else
                 {
                     Flick.error(line, "Unexpected charater.");
                     break;    
                 }
         }
+    }
+
+    private void identifier() 
+    {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
     }
 
     private void number() 
@@ -147,6 +185,18 @@ public class Scanner
     private boolean isDigit(char c) 
     {
         return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) 
+    {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) 
+    {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isAtEnd() 
