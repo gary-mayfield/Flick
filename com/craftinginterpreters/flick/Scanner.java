fc7ad26cd4c6ad4fcc1fb37dd26d8196e98562f6
gary-mayfield <gary.mayfield@hotmail.co.uk>
2022-02-13 11:27:32 +0000

    Up to 4.7 reserved words and identifiers.

--- com/craftinginterpreters/flick/Scanner.java
+++ com/craftinginterpreters/flick/Scanner.java
@@ -32,11 +32,141 @@ public class Scanner
 
     private void scanToken() 
     {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
 
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if(match('/'))
+                {
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                }
+                else
+                {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace characters.
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if(isDigit(c))
+                {
+                    number();
+                }
+                else
+                {
+                    Flick.error(line, "Unexpected charater.");
+                    break;    
+                }
+        }
+    }
+
+    private void number() 
+    {
+        while(isDigit(peek())) advance();
+
+        if(peek() == '.' && isDigit(peekNext()))
+        {
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() 
+    {
+        while(peek() != '"' && !isAtEnd())
+        {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd())
+        {
+            Flick.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private boolean match(char expected) 
+    {
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() 
+    {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() 
+    {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) 
+    {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() 
     {
         return current >= source.length();
+    }
+
+    private char advance() 
+    {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) 
+    {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) 
+    {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
